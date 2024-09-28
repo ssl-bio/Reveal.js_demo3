8e0368361ebdac6845e82c00667ff8b9466cc7f0
saul-sl <ssl-work@proton.me>
2024-09-28 00:17:57 -0400

    Improved responsiveness for large screens and use of tailwind
    - Added more responsive text sizes
    - Custom CSS was build
    - Fixed use of speaker-view
    - Removed div and other unnecessary classes
    - Improved use of flexbox
    - Modified descriptive content

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./**/*.{html,js}"],
+    theme: {
+	extend: {
+	    screens: {
+		'1080p': '1920px',
+		'2k': '2048px',
+		'1440p': '2560px',
+		'2160p': '3840px',
+		'4k': '4096px',
+	    },
+	    fontSize: {
+		'2xs': ['0.7rem', '0.95rem'],
+		'3xs': ['0.575rem', '0.8rem'],
+		'4xs': ['0.45rem', '0.5rem'],
+		'5xs': ['0.325rem', '0.45rem'],
+		'6xs': ['0.2rem', '0.3rem'],
+		'7xs': ['0.075rem', '0.11rem'],
+	    },
+	},
+    },
+    safelist: [
+	'1080p',
+	'2k',
+	'1440p',
+	'2160p',
+	'4k',
+    ],
+    plugins: [],
+}
+
